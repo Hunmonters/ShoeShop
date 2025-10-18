b6d73da7547a09620ceed18b4bf10a52c786ad76
Hunmonters <khat5573@gmail.com>
2025-10-18 09:56:44 +0700

    update Login-home

--- src/main/java/com/shoeshop/config/MailConfig.java
+++ src/main/java/com/shoeshop/config/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("your-email@gmail.com");
-        mailSender.setPassword("your-app-password");
+        mailSender.setUsername("ShoeShop@gmail.com");
+        mailSender.setPassword("123");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
